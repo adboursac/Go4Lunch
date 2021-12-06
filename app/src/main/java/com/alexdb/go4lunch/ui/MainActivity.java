91d36871f07a65d066eb9f4b607acf2c23ee5cd5
Alexandre de Boursac <alexdb.freelance@gmail.com>
2021-12-06 13:32:17 +0100

    Google login, Theme and Resources

--- app/src/main/java/com/alexdb/go4lunch/ui/MainActivity.java
+++ app/src/main/java/com/alexdb/go4lunch/ui/MainActivity.java
@@ -1,22 +1,90 @@
 package com.alexdb.go4lunch.ui;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.alexdb.go4lunch.R;
 import com.alexdb.go4lunch.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupListeners();
     }
 
     @Override
     ActivityMainBinding getViewBinding() {
         return ActivityMainBinding.inflate(getLayoutInflater());
+    }
+
+    private void setupListeners(){
+        // Login Button
+        mBinding.loginButton.setOnClickListener(view -> startSignInActivity());
+    }
+
+    private void startSignInActivity(){
+        // Authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+                //new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.AppTheme_NoTitle)
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false, true)
+                //.setLogo(R.drawable.ic_go4lunch_logo)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    //Sign In Launcher
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            showSnackBar(getString(R.string.connection_succeed));
+        } else {
+            // Sign in failed
+            if (response == null) {
+                showSnackBar(getString(R.string.error_authentication_canceled));
+            } else if (response.getError()!= null) {
+                if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
+                    showSnackBar(getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+
+    // Show Snack Bar with a message
+    private void showSnackBar( String message){
+        Snackbar.make(mBinding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 }
