09123f5d1b63c9f2cadee0e91da53758fde247ee
Alexandre de Boursac <alexdb.freelance@gmail.com>
2022-01-16 20:16:04 +0100

    Update rating stars conversion

--- app/src/main/java/com/alexdb/go4lunch/ui/helper/RatingHelper.java
+++ app/src/main/java/com/alexdb/go4lunch/ui/helper/RatingHelper.java
@@ -5,28 +5,28 @@ import android.widget.ImageView;
 
 public class RatingHelper {
 
-    //[0,2.5[ No star, [2.5, 3.5[ = 1 star, [3.5,4.5[ = 2 stars, [4.5,5] = 3 stars
+    //[0,2[ No star, [2, 3[ = 1 star, [3, 4[ = 2 stars, [4,5] = 3 stars
     public static void displayStarsScheme(double rate, ImageView star1, ImageView star2, ImageView star3) {
-        int rate_int = (int) Math.round(rate);
+        int rate_int = (int) Math.floor(rate);
 
         switch (rate_int) {
             case 0:
             case 1:
-            case 2:
                 star1.setVisibility(View.GONE);
+                star2.setVisibility(View.GONE);
+                star3.setVisibility(View.GONE);
+                break;
+            case 2:
+                star1.setVisibility(View.VISIBLE);
                 star2.setVisibility(View.GONE);
                 star3.setVisibility(View.GONE);
                 break;
             case 3:
                 star1.setVisibility(View.VISIBLE);
-                star2.setVisibility(View.GONE);
-                star3.setVisibility(View.GONE);
-                break;
-            case 4:
-                star1.setVisibility(View.VISIBLE);
                 star2.setVisibility(View.VISIBLE);
                 star3.setVisibility(View.GONE);
                 break;
+            case 4:
             case 5:
                 star1.setVisibility(View.VISIBLE);
                 star2.setVisibility(View.VISIBLE);
